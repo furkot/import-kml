b254df3308a1b40c3b82c9b356a93ea730bb58ea
Damian Krzeminski <pirxpilot@furkot.com>
2022-01-04 17:01:54 +0100

    replace mocha with tape
    also remove should

--- test/import.js
+++ test/import.js
@@ -1,84 +1,76 @@
-var should = require('should');
+var test = require('tape');
 var fs = require('fs');
 var parse = require('..');
 
-describe('furkot import kml', function() {
-  it('should parse kml', function(done) {
-    var stream = fs.createReadStream(__dirname + '/fixtures/usa.kml');
-    parse(stream, function(err, trip) {
-      var expected = require('./fixtures/usa.json');
+test('should parse kml', function (t) {
+  var stream = fs.createReadStream(__dirname + '/fixtures/usa.kml');
+  parse(stream, function (err, trip) {
+    var expected = require('./fixtures/usa.json');
 
-      should.not.exist(err);
-      should.exist(trip);
-      trip.should.eql(expected);
-      done();
-    });
+    t.error(err);
+    t.deepEqual(trip, expected);
+    t.end();
   });
+});
 
-  it('should parse kml with folders', function(done) {
-    var stream = fs.createReadStream(__dirname + '/fixtures/et.kml');
-    parse(stream, function(err, trip) {
-      var expected = require('./fixtures/et.json');
+test('should parse kml with folders', function (t) {
+  var stream = fs.createReadStream(__dirname + '/fixtures/et.kml');
+  parse(stream, function (err, trip) {
+    var expected = require('./fixtures/et.json');
 
-      should.not.exist(err);
-      should.exist(trip);
-      trip.should.eql(expected);
-      done();
-    });
+    t.error(err);
+    t.deepEqual(trip, expected);
+    t.end();
   });
+});
 
-  it('should parse kml with nested folders', function(done) {
-    var stream = fs.createReadStream(__dirname + '/fixtures/nested.kml');
-    parse(stream, function(err, trip) {
-      var expected = require('./fixtures/nested.json');
+test('should parse kml with nested folders', function (t) {
+  var stream = fs.createReadStream(__dirname + '/fixtures/nested.kml');
+  parse(stream, function (err, trip) {
+    var expected = require('./fixtures/nested.json');
 
-      should.not.exist(err);
-      should.exist(trip);
-      trip.should.eql(expected);
-      done();
-    });
+    t.error(err);
+    t.deepEqual(trip, expected);
+    t.end();
   });
+});
 
-  it('should parse kml no container', function(done) {
-    var stream = fs.createReadStream(__dirname + '/fixtures/placemark.kml');
-    parse(stream, function(err, trip) {
-      var expected = {
-        stops: [{
-          coordinates: {
-            lat: 37.422069,
-            lon: -122.087461
-          },
-          name: 'My office',
-          notes: 'This is the location of my office.'
-        }]
-      };
+test('should parse kml no container', function (t) {
+  var stream = fs.createReadStream(__dirname + '/fixtures/placemark.kml');
+  parse(stream, function (err, trip) {
+    var expected = {
+      stops: [{
+        coordinates: {
+          lat: 37.422069,
+          lon: -122.087461
+        },
+        name: 'My office',
+        notes: 'This is the location of my office.'
+      }]
+    };
 
-      should.not.exist(err);
-      should.exist(trip);
-      trip.should.eql(expected);
-      done();
-    });
+    t.error(err);
+    t.deepEqual(trip, expected);
+    t.end();
   });
+});
 
-  it('empty KML', function(done) {
-    var stream = fs.createReadStream(__dirname + '/fixtures/empty.kml');
-    parse(stream, function(err, trip) {
-      should.not.exist(err);
-      trip.should.eql({
-        destination: 'empty'
-      });
-      done();
-    });
+test('empty KML', function (t) {
+  var stream = fs.createReadStream(__dirname + '/fixtures/empty.kml');
+  parse(stream, function (err, trip) {
+    t.error(err);
+    t.deepEqual(trip, { destination: 'empty' });
+    t.end();
   });
+});
 
-  it('should raise error on invalid XML file', function(done) {
-    var stream = fs.createReadStream(__dirname + '/fixtures/invalid.kml');
-    parse(stream, function(err, trip) {
-      should.exist(err);
-      err.should.have.property('err', 'invalid');
-      err.should.have.property('message');
-      should.not.exist(trip);
-      done();
-    });
+test('should raise error on invalid XML file', function (t) {
+  var stream = fs.createReadStream(__dirname + '/fixtures/invalid.kml');
+  parse(stream, function (err, trip) {
+    t.notLooseEqual(err, null, 'error should exists');
+    t.equal(err.err, 'invalid');
+    t.ok('message' in err, 'should have property message');
+    t.looseEqual(trip, null, 'trips should not exist');
+    t.end();
   });
 });
