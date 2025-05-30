5bb2b8dea7f26c0febda3f827fb0ee168f471cba
Damian Krzeminski <pirxpilot@furkot.com>
2025-05-29 19:56:27 -0600

    use `biome` as a linter and formatter

--- test/import.js
+++ test/import.js
@@ -8,59 +8,66 @@ const parse = require('..');
 /* global TextDecoderStream */
 
 async function createFromStream(file) {
-  const name = path.join(__dirname, file);
+  const name = path.resolve(__dirname, file);
   const handle = await fs.open(name);
   return handle.readableWebStream().pipeThrough(new TextDecoderStream());
 }
 
+async function readJSON(file) {
+  const name = path.resolve(__dirname, file);
+  return JSON.parse(await fs.readFile(name, 'utf8'));
+}
+
 test('should parse kml', async function () {
-  const stream = await createFromStream('/fixtures/usa.kml');
+  const stream = await createFromStream('./fixtures/usa.kml');
   const trip = await parse(stream);
-  const expected = require('./fixtures/usa.json');
+  const expected = await readJSON('./fixtures/usa.json');
 
   assert.deepEqual(trip, expected);
 });
 
 test('should parse kml with folders', async function () {
-  const stream = await createFromStream('/fixtures/et.kml');
+  const stream = await createFromStream('./fixtures/et.kml');
   const trip = await parse(stream);
-  const expected = require('./fixtures/et.json');
+  const expected = await readJSON('./fixtures/et.json');
 
   assert.deepEqual(trip, expected);
 });
 
 test('should parse kml with nested folders', async function () {
-  const stream = await createFromStream('/fixtures/nested.kml');
+  const stream = await createFromStream('./fixtures/nested.kml');
   const trip = await parse(stream);
-  const expected = require('./fixtures/nested.json');
+  const expected = await readJSON('./fixtures/nested.json');
 
   assert.deepEqual(trip, expected);
 });
 
 test('should parse kml no container', async function () {
-  const stream = await createFromStream('/fixtures/placemark.kml');
+  const stream = await createFromStream('./fixtures/placemark.kml');
   const trip = await parse(stream);
   const expected = {
-    stops: [{
-      coordinates: {
-        lat: 37.422069,
-        lon: -122.087461
-      },
-      name: 'My office',
-      notes: 'This is the location of my office.'
-    }]
+    stops: [
+      {
+        coordinates: {
+          lat: 37.422069,
+          lon: -122.087461
+        },
+        name: 'My office',
+        notes: 'This is the location of my office.'
+      }
+    ]
   };
 
   assert.deepEqual(trip, expected);
 });
 
 test('empty KML', async function () {
-  const stream = await createFromStream('/fixtures/empty.kml');
+  const stream = await createFromStream('./fixtures/empty.kml');
   const trip = await parse(stream);
   assert.deepEqual(trip, { destination: 'empty' });
 });
 
 test('should raise error on invalid XML file', async function () {
-  const stream = await createFromStream('/fixtures/invalid.kml');
+  const stream = await createFromStream('./fixtures/invalid.kml');
   await assert.rejects(parse(stream));
 });
